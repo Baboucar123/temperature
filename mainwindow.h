390d16c6bbcdedeccbe3c7d0ed102d4c5673323e
Aspirant <coly.baboucar.ab@gmail.com>
2024-10-11 17:11:20 +0200

    Add files via upload

---
+++ mainwindow.h
@@ -0,0 +1,31 @@
+#include <QtWidgets> /* tous les widgets de Qt5 */
+#include <QtGui> /* tous les widgets de Qt4 */
+#define CELCIUS_FARENHEIT 0
+#define FARENHEIT_CELCIUS 1
+class MaFenetre : public QDialog
+{
+Q_OBJECT
+// Membre(s) public(s)
+public:
+MaFenetre( QWidget *parent = 0 );
+// Membre(s) privé(s)
+private:
+// Les widgets
+QLineEdit *valeur;
+QLabel *resultat;
+QLabel *unite;
+QComboBox *choixConversion;
+QPushButton *bConvertir;
+QPushButton *bQuitter;
+QDoubleValidator *doubleValidator;
+void afficherUnite();
+
+   // Mécanisme(s) Qt
+signals:
+   void actualiser();
+
+private slots:
+   void convertir();
+   void permuter();
+
+};
